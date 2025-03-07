6a823b6be6c9fbd0f0e9b5d901a47616441c7287
Khushi2405 <khushigandhi2405@gmail.com>
2025-03-07 17:24:48 -0500

    cleaned the code

--- src/main/java/com/database/finalproject/DatabaseFinalProjectApplication.java
+++ src/main/java/com/database/finalproject/DatabaseFinalProjectApplication.java
@@ -22,8 +22,8 @@ public class DatabaseFinalProjectApplication {
 	public static void main(String[] args) throws IOException {
 		SpringApplication.run(DatabaseFinalProjectApplication.class, args);
 		BufferManager bufferManager = new BufferManagerImpl(10);
-		//Utilities.loadDataset(bufferManager, "src/main/resources/static/title.basics.tsv");
-		//((BufferManagerImpl) bufferManager).readPage(1);
+//		Utilities.loadDataset(bufferManager, "src/main/resources/static/title.basics.tsv");
+//		((BufferManagerImpl) bufferManager).readPage(1);
 //		Page page = bufferManager.createPage();
 //		int pageId = page.getPid();
 //		System.out.println("print in main " + pageId);
@@ -75,29 +75,55 @@ public class DatabaseFinalProjectApplication {
 //			System.out.println("File does not exist.");
 //		}
 
-		for(int i = 1 ; i < 12; i++){
-			bufferManager.getPage(i);
-			bufferManager.unpinPage(i);
-		}
+//		for(int i = 1 ; i < 12; i++){
+//			bufferManager.getPage(i);
+//			bufferManager.unpinPage(i);
+//		}
+//
+//		bufferManager.getPage(2);
+//		bufferManager.getPage(1);
+//		Page page2 = bufferManager.createPage();
+//		String id = "1234new";
+//		String title = "new page ";
+//		byte[] movieId = id.getBytes(StandardCharsets.UTF_8);
+//		byte[] movieTitle = title.getBytes(StandardCharsets.UTF_8);
+//		Row row = new Row(movieId, movieTitle);
+//		page2.insertRow(row);
+//		int pageId = page2.getPid();
+//		bufferManager.markDirty(pageId);
+//		System.out.println(pageId);
+//		bufferManager.unpinPage(pageId);
+//		for(int i = 1 ; i < 12; i++){
+//			bufferManager.getPage(i);
+//			bufferManager.unpinPage(i);
+//		}
+//		page2 = bufferManager.getPage(pageId);
+//		page2.insertRow(row);
+//		bufferManager.markDirty(pageId);
+//
+//		Page page3 = bufferManager.createPage();
+//		int pageId3 = page3.getPid();
+//		page3.insertRow(row);
+//		bufferManager.unpinPage(pageId3);
+//
+//		for(int i = 1 ; i < 12; i++){
+//			bufferManager.getPage(i);
+//			bufferManager.unpinPage(i);
+//		}
+//
+//		bufferManager.unpinPage(pageId);
+//
+//		for(int i = 1 ; i < 12; i++){
+//			bufferManager.getPage(i);
+//			bufferManager.unpinPage(i);
+//		}
+//
+//		bufferManager.getPage(pageId);
+//		bufferManager.getPage(pageId3);
+//
 
-		bufferManager.getPage(2);
-		bufferManager.getPage(1);
-		Page page2 = bufferManager.createPage();
-		String id = "1234new";
-		String title = "new page ";
-		byte[] movieId = id.getBytes(StandardCharsets.UTF_8);
-		byte[] movieTitle = title.getBytes(StandardCharsets.UTF_8);
-		Row row = new Row(movieId, movieTitle);
-		page2.insertRow(row);
-		int pageId = page2.getPid();
-		bufferManager.markDirty(pageId);
-		System.out.println(pageId);
-		bufferManager.unpinPage(pageId);
-		for(int i = 1 ; i < 12; i++){
-			bufferManager.getPage(i);
-			bufferManager.unpinPage(i);
-		}
-		((BufferManagerImpl) bufferManager).readPage(pageId);
+
+//		((BufferManagerImpl) bufferManager).readPage(pageId);
 
 
 
