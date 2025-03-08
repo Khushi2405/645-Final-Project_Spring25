0d6fe846e60e91fb1108604c4fdbe06869901523
vishakha <vishakhanm25@gmail.com>
2025-03-07 19:50:47 -0500

    clean code and fix mark dirty inside create page

--- src/test/java/com/database/finalproject/DatabaseFinalProjectApplicationTests.java
+++ src/test/java/com/database/finalproject/DatabaseFinalProjectApplicationTests.java
@@ -20,47 +20,4 @@ class DatabaseFinalProjectApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
-	private BufferManager bufferManager;
-
-	@BeforeEach
-	void setUp() {
-		bufferManager = new BufferManagerImpl(5);
-	}
-
-	@Test
-	void testCreatePage() {
-		Page page = bufferManager.createPage();
-		assertNotNull(page, "Page should be created successfully");
-	}
-
-	@Test
-	void testInsertRow() {
-		Page page = bufferManager.createPage();
-		Row row = new Row("tt1111111".getBytes(StandardCharsets.UTF_8),
-				"Test Movie Insert".getBytes(StandardCharsets.UTF_8));
-
-		int rowId = page.insertRow(row);
-		assertEquals(0, rowId - 1, "Row should be inserted at index 0");
-	}
-
-	@Test
-	void testFetchPage() {
-		Page createdPage = bufferManager.createPage();
-		int pageId = createdPage.getPid();
-
-		Page fetchedPage = bufferManager.getPage(pageId);
-		assertNotNull(fetchedPage, "Fetched page should not be null");
-		assertEquals(pageId, fetchedPage.getPid(), "Fetched page ID should match");
-	}
-
-	// @Test
-	// void testMarkDirtyOnCreatePage() {
-	// Page page = bufferManager.createPage();
-	// int pageId = page.getPid();
-
-	// bufferManager.markDirty(pageId);
-	// // Verify dirty page logic, could be a flag inside BufferManager
-	// }
-
 }
