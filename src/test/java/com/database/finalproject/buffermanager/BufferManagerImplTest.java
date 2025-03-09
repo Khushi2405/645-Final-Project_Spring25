353bf8ad75c3ca77e4702edf419b2a4c6bde73d6
vishakha <vishakhanm25@gmail.com>
2025-03-08 20:08:16 -0500

    UT: createPage, bufferFull, no exception thrown

--- src/test/java/com/database/finalproject/buffermanager/BufferManagerImplTest.java
+++ src/test/java/com/database/finalproject/buffermanager/BufferManagerImplTest.java
@@ -1,8 +1,7 @@
 package com.database.finalproject.buffermanager;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 import java.nio.charset.StandardCharsets;
 
@@ -22,25 +21,18 @@ class BufferManagerImplTest {
     }
 
     // page
+    //createPage tests
     @Test
     void testCreatePage() {
+        // create a new page
         Page page = bufferManager.createPage();
         assertNotNull(page, "Page should be created successfully");
         assertTrue(page.getPid() >= 0, "Page ID should be valid");
     }
 
     @Test
-    void testFetchPage() {
-        Page createdPage = bufferManager.createPage();
-        int pageId = createdPage.getPid();
-
-        Page fetchedPage = bufferManager.getPage(pageId);
-        assertNotNull(fetchedPage, "Fetched page should not be null");
-        assertEquals(pageId, fetchedPage.getPid(), "Fetched page ID should match");
-    }
-
-    @Test
     void testBufferFullForCreatePage() {
+        // buffer full for createPage
         BufferManagerImpl bufferManagerSpy = spy(new BufferManagerImpl(5));
         for (int i = 1; i <= 5; i++) {
             Page createdPage = bufferManagerSpy.createPage();
@@ -50,8 +42,42 @@ class BufferManagerImplTest {
         assertNull(createdPageExtra, "Page should not be create, response should be NULL");
     }
 
+    // getPage tests
+    @Test
+    void testFetchPage() {
+        // page already in buffer pool
+        Page createdPage = bufferManager.createPage();
+        int pageId = createdPage.getPid();
+
+        Page fetchedPage = bufferManager.getPage(pageId);
+        assertNotNull(fetchedPage, "Fetched page should not be null");
+        assertEquals(pageId, fetchedPage.getPid(), "Fetched page ID should match");
+    }
+
+    @Test
+    void testFetchPageNotInBuffer() {
+        // page not in buffer pool
+        for(int i = 1; i <= 4; i++) {
+            Page createdPage = bufferManager.createPage();
+        }
+        Page page5 = bufferManager.createPage();
+        int pageId5 = page5.getPid();
+        bufferManager.unpinPage(pageId5);
+
+        // evict page 5
+        Page page6 = bufferManager.createPage();
+        int pageId6 = page6.getPid();
+        bufferManager.unpinPage(pageId6);
+
+        // get page 5, evict 6
+        Page fetchedPage = bufferManager.getPage(pageId5);
+        assertNotNull(fetchedPage, "Fetched page should not be null");
+        assertEquals(pageId5, fetchedPage.getPid(), "Fetched page ID should match");
+    }
+
     @Test
     void testBufferFullForGetPage() {
+        // buffer full on getPage
         BufferManagerImpl bufferManagerSpy = spy(new BufferManagerImpl(5));
         for (int i = 1; i <= 4; i++) {
             Page createdPage = bufferManagerSpy.createPage();
@@ -69,6 +95,7 @@ class BufferManagerImplTest {
 
     @Test
     void testWritePageCall() {
+        //empty page writes
         BufferManagerImpl bufferManagerSpy = spy(new BufferManagerImpl(5));
         for (int i = 1; i <= 4; i++) {
             Page createdPage = bufferManagerSpy.createPage();
@@ -83,6 +110,7 @@ class BufferManagerImplTest {
 
     @Test
     void testMarkDirtyOnCreatePage() {
+        // page to be marked dirty on createPage
         BufferManagerImpl bufferManagerSpy = spy(new BufferManagerImpl(5));
 
         Page newPage = bufferManagerSpy.createPage();
@@ -122,10 +150,30 @@ class BufferManagerImplTest {
 
         Row fetchedRow = page.getRow(rowId);
         assertNotNull(fetchedRow, "Inserted row should be retrievable");
+
+        assertFalse(page.isFull(), "Page is not full.");
     }
 
     @Test
-    void testwriteToBinaryFileInsertRow() {
+    void testPageFull() {
+        Page page = bufferManager.createPage();
+        for(int i = 1; i <= 105; i++) {
+            Row row = new Row("tt1111111".getBytes(StandardCharsets.UTF_8),
+                    "Test Movie Insert".getBytes(StandardCharsets.UTF_8));
+            int rowId = page.insertRow(row);
+        }
+
+        assertTrue(page.isFull(), "Page is full.");
+
+        Row row = new Row("tt1111111".getBytes(StandardCharsets.UTF_8),
+                "Test Movie Insert".getBytes(StandardCharsets.UTF_8));
+        int rowId = page.insertRow(row);
+        assertEquals(-1, rowId, "Should return -1 on new insertion if page is full");
+    }
+
+    @Test
+    void testWriteToBinaryFileInsertRow() {
+        // write to binary file when page with rows inserted and marked dirty is evicted
         BufferManagerImpl bufferManagerSpy = spy(new BufferManagerImpl(5));
         // make 4 pages and pin
         for (int i = 1; i <= 4; i++) {
@@ -166,6 +214,14 @@ class BufferManagerImplTest {
         assertEquals("No page with this ID - " + invalidPageId, exception.getMessage());
     }
 
+    @Test
+    void testMarkDirtyDoesNotThrowError() {
+        Page newPage = bufferManager.createPage();
+        int pageId = newPage.getPid();
+        assertDoesNotThrow(() -> bufferManager.markDirty(pageId),
+                "markDirty should not throw an error when page exists in buffer");
+    }
+
     // unpin Exception
     @Test
     void testUnpinPageThrowsExceptionWhenPageNotFound() {
@@ -176,6 +232,13 @@ class BufferManagerImplTest {
         });
 
         assertEquals("No page with this ID - " + invalidPageId, exception.getMessage());
+    }
+    @Test
+    void testUnpinPageDoesNotThrowError() {
+        Page newPage = bufferManager.createPage();
+        int pageId = newPage.getPid();
+        assertDoesNotThrow(() -> bufferManager.unpinPage(pageId),
+                "unpinPage should not throw an error when page exists in buffer");
     }
 
 }
